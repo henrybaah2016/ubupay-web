3c6001126d47f17cec61a0ce186650858a23a8f8
digitalnativesltd <henrybaah2016@gmail.com>
2025-01-06 16:17:15 -0800

    update

--- src/app/dashboard/screens/component/quick-send.tsx
+++ src/app/dashboard/screens/component/quick-send.tsx
@@ -20,30 +20,35 @@ const QuickSend = () => {
     ];
 
     return (
-        <div className="flex flex-col mx-[30px] my-[30px]">
-              <div className="flex justify-between">
-                    <h4 className="text-lg text-[#34342A] font-semibold">Quick Send</h4>
-                    <h6 className="text-sm text-[#A5A5A5] font-semibold">View All</h6>
-                </div>
-            <div className="flex justify-left space-x-10 mx-[0px] my-[20px]">
+        <div className="flex flex-col mx-4 sm:mx-6 md:mx-8 lg:mx-10 xl:mx-12 my-4 sm:my-6 md:my-8 lg:my-10 xl:my-12">
+            <div className="flex justify-between items-center">
+                <h4 className="text-base sm:text-lg md:text-xl lg:text-2xl text-[#34342A] font-semibold">
+                    Quick Send
+                </h4>
+                <h6 className="text-xs sm:text-sm md:text-base lg:text-lg text-[#A5A5A5] font-semibold cursor-pointer">
+                    View All
+                </h6>
+            </div>
 
-              
+            
+            <div className="flex flex-wrap justify-start gap-4 sm:gap-6 md:gap-8 lg:gap-10 mt-4 sm:mt-6 md:mt-8">
                 {senders.map((sender, index) => (
                     <div
                         key={index}
-                        className={`flex items-center w-[250px] space-x-4 px-4 py-6 rounded-[16px] text-white ${sender.color}`}
+                        className={`flex items-center w-full sm:w-[200px] md:w-[220px] lg:w-[250px] px-4 py-4 sm:py-6 rounded-lg text-white ${sender.color}`}
                     >
                         <img
                             src={sender.image}
                             alt={sender.name}
-                            className="w-10 h-10 rounded-full"
+                            className="w-8 sm:w-10 md:w-12 lg:w-14 h-8 sm:h-10 md:h-12 lg:h-14 rounded-full"
                         />
-                        <span className="text-[16px] text-[#6C6C6C] font-semibold">{sender.name}</span>
+                        <span className="ml-4 text-sm sm:text-base md:text-lg text-[#6C6C6C] font-semibold">
+                            {sender.name}
+                        </span>
                     </div>
                 ))}
             </div>
         </div>
-
     );
 };
 
