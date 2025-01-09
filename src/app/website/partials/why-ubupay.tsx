aeea171064d708cad5dd13ec34b3216323ddab0e
digitalnativesltd <henrybaah2016@gmail.com>
2025-01-08 18:36:21 -0800

    update

--- src/app/website/partials/why-ubupay.tsx
+++ src/app/website/partials/why-ubupay.tsx
@@ -5,11 +5,11 @@ const WhyUbupay = () => {
         <section className="bg-primary w-full  mt-[0px] xl:mt-[140px] lg:mt-[140px] md:mt-[140px] py-[0px] xl:py-[100px] lg:py-[100px] md:py-[100px] rounded-[0px]">
             <div className="flex flex-col  md:flex-col xl:flex-row lg:flex-row px-6 mx-[0px] xl:mx-[70px] md:mx-[30px] lg:mx-[30px] py-10 ">
                 <div className="w-full xl:w-[45%] md:w-[90%]  lg:w-[40%] flex flex-col justify-center items-start  mb-6 md:mb-0">
-                    <h1 className="text-[#ffffff] xl:pr-[160px] lg:pr-[100px] md:pt-[50px] lg:pt-[50px] xl:pt-[50px] pt-[30px] leading-tight lg:leading-[53px] xl:leading-[50px] xl:pb-[5px] md:pb-[10px] lg:pb-0 pb-[10px] pt-0 lg:pt-0 xl:pt-2 md:pt-[100px] lg:text-[40px] xl:text-[40px]  md:text-[40px] font-bold text-2xl">
+                    <h1 className="text-[#ffffff] xl:pr-[40px] lg:pr-[100px] md:pt-[50px] lg:pt-[50px] xl:pt-[50px] pt-[30px] leading-tight lg:leading-[53px] xl:leading-[50px] xl:pb-[5px] md:pb-[10px] lg:pb-0 pb-[10px] pt-0 lg:pt-0 xl:pt-2 md:pt-[100px] lg:text-[40px] xl:text-[40px]  md:text-[40px] font-bold text-2xl">
                         Seamless secure
                         & efficient money transfers
                     </h1>
-                    <p className="pt-2 xl:pr-[100px]  lg:pr-[100px]  text-[#ECECEC] text-[18px] leading-snug font-semibold md:text-[17px] xl:text-[17px] lg:text-[17px]">
+                    <p className="pt-2 xl:pr-[80px]  lg:pr-[100px]  text-[#ECECEC] text-[18px] leading-snug font-semibold md:text-[17px] xl:text-[17px] lg:text-[17px]">
                         UbuPay enables affordable, seamless money transfers to Africa for loved ones and businesses.
                     </p>
                     <button className="px-6 xl:mt-[50px] lg:mt-[50px] md:mt-[50px] md:mb-[50px] mt-[30px] py-2 btn-readmore  transition">
